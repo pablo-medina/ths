3f69780053d7c0a71d9e251ffbb493df82f10aa1
Pablo Medina <pablo_medina@outlook.com>
2023-06-23 00:32:08 -0300

    Improved command line parsing and help message

--- ths.go
+++ ths.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,10 +10,19 @@ import (
 )
 
 func main() {
-	// Get command line arguments
-	dir := flag.String("dir", ".", "Hosting directory")
-	port := flag.String("port", "8080", "Port")
+	// Define command line flags
+	dir := flag.String("dir", "", "Hosting directory (required)")
+	port := flag.String("port", "8080", "Server port")
+	help := flag.Bool("help", false, "Show help")
+
+	// Parse command line flags
 	flag.Parse()
+
+	// Display help if requested or if directory is not provided
+	if *help || *dir == "" {
+		printHelp()
+		return
+	}
 
 	// Handler to serve Angular static files
 	staticHandler := http.FileServer(http.Dir(*dir))
@@ -25,6 +35,17 @@ func main() {
 
 	// Start the server on the specified port
 	log.Println("Server started at http://localhost:" + *port)
-	log.Println("Reading files from: " + *dir)
+	log.Println("Hosting files from: " + *dir)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
+func printHelp() {
+	fmt.Println("Usage: ths [options]")
+	fmt.Println()
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println("  ths --dir=/path/to/your/angular-app --port=8000")
+	fmt.Println("    - Starts the server on port 8000, hosting the Angular application in /path/to/your/angular-app.")
 }
