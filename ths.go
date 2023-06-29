61e837f726b3f46cfe2b86e6070ea86895e40958
Pablo Medina <pablo_medina@outlook.com>
2023-06-28 22:02:47 -0300

    Added HTTPS support. Made CORS optional (disabled by default).

--- ths.go
+++ ths.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/rs/cors"
 )
@@ -13,6 +14,10 @@ func main() {
 	// Define command line flags
 	dir := flag.String("dir", "", "Hosting directory (required)")
 	port := flag.String("port", "8080", "Server port")
+	https := flag.Bool("https", false, "Enable HTTPS")
+	certFile := flag.String("cert", "", "Certificate file path")
+	keyFile := flag.String("key", "", "Key file path")
+	corsEnabled := flag.Bool("cors", false, "Enable CORS")
 	help := flag.Bool("help", false, "Show help")
 
 	// Parse command line flags
@@ -27,16 +32,41 @@ func main() {
 	// Handler to serve Angular static files
 	staticHandler := http.FileServer(http.Dir(*dir))
 
+	// Handler for catchinf all request and servince index.html
+	indexHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(*dir, "index.html"))
+	})
+
+	// Combine static and index handlers
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		staticHandler.ServeHTTP(w, r)
+		indexHandler.ServeHTTP(w, r)
+	})
+
 	// CORS configuration
-	corsHandler := cors.Default().Handler(staticHandler)
+	var corsHandler http.Handler = handler
+	corsStatus := "DISABLED"
+	if *corsEnabled {
+		corsHandler = cors.Default().Handler(handler)
+		corsStatus = "ENABLED"
+	}
 
 	// Root path
 	http.Handle("/", corsHandler)
 
 	// Start the server on the specified port
-	log.Println("Server started at http://localhost:" + *port)
-	log.Println("Hosting files from: " + *dir)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	if *https {
+		log.Println("Server started at https://localhost:" + *port)
+		log.Println("CORS is " + corsStatus)
+		log.Println("Hosting files from: " + *dir)
+		log.Println("Certificate File: " + *certFile)
+		log.Println("Private Key File: " + *keyFile)
+		log.Fatal(http.ListenAndServeTLS(":"+*port, *certFile, *keyFile, nil))
+	} else {
+		log.Println("Server started at http://localhost:" + *port)
+		log.Println("Hosting files from: " + *dir)
+		log.Fatal(http.ListenAndServe(":"+*port, nil))
+	}
 }
 
 func printHelp() {
